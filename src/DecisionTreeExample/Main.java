f67c2de4d4697d4f1047bcff53a191a771abf14a
Hadi0000 <44855848+Hadi0000@users.noreply.github.com>
2018-12-02 16:14:31 +0600

    Add files via upload

---
+++ src/DecisionTreeExample/Main.java
@@ -0,0 +1,10 @@
+package DecisionTreeExample;
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Controller c = new Controller();
+		c.getInputData("0data2.txt");
+	}	
+}
---
+++ src/DecisionTreeExample/TreeNode.java
@@ -0,0 +1,10 @@
+package DecisionTreeExample;
+
+import java.util.HashMap;
+
+public class TreeNode {
+	int index;
+	HashMap<String,TreeNode> child = new HashMap <String,TreeNode>();
+	boolean isLeaf;
+	String answer;
+}
